e553f675c933ef80dc1a7fec5a7d2058edbcc811
kfranic1 <56261743+kfranic1@users.noreply.github.com>
2021-12-11 20:39:44 +0100

    Update misc-template.cpp

--- misc-template.cpp
+++ misc-template.cpp
@@ -1,9 +1,25 @@
 #include <bits/stdc++.h>
-#define ll long long
 
 using namespace std;
 
-const int MOD = 1000000007;
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define FOR(i, a, b) for (int i = (a); i < int(b); ++i)
+#define REP(i, n) FOR(i, 0, n)
+#define __ << ' ' <<
+
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+
+const int MOD = 1e9 + 7;
+const int MAXN = 3e5 + 5;
+const llint INF = 1e18;
+
+const long double E = 1e-9;
 
 int main(void) {
     #ifdef DEBUG
@@ -13,9 +29,7 @@ int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-
-
-
-
+    //kod
+    
     return 0;
 }
